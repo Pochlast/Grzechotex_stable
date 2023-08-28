3c87667e8f22244c669c5b8339ebda753242aeaa
Pochlast <121972229+Pochlast@users.noreply.github.com>
2023-08-28 22:37:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -238,7 +238,7 @@ function coll_det_bb(b1, b2) {
 function Horizontal_coll(b) {
     if (b.soundAbilityHorizontal && Math.abs(b.vel.x) > vel_lim) {
         b.soundAbilityHorizontal = false;
-        let loudness =((Math.abs(b.vel.x)<vel_lim_loudness) ? Math.abs(b.vel.x)*100/vel_lim_loudness : 100);
+        let loudness =((Math.abs(b.vel.x)<vel_lim_loudness) ? Math.abs(b.vel.x)/vel_lim_loudness : 1);
         if (b.pos.x > 1 / 3 * wall_width && b.pos.x < 2 / 3 * wall_width) { new Audio(soundFile1).volume(loudness).play(); }
         new Audio(soundFile2).play();
     }
@@ -247,7 +247,7 @@ function Horizontal_coll(b) {
 function Vertical_coll(b) {
     if (b.soundAbilityHorizontal && Math.abs(b.vel.y) > vel_lim) {
         b.soundAbilityHorizontal = false;
-        let loudness =((Math.abs(b.vel.x)<vel_lim_loudness) ? Math.abs(b.vel.x)*100/vel_lim_loudness : 100);
+        let loudness =((Math.abs(b.vel.x)<vel_lim_loudness) ? Math.abs(b.vel.x)/vel_lim_loudness : 1);
         if (b.pos.y > 1 / 3 * wall_height && b.pos.y < 2 / 3 * wall_height) { new Audio(soundFile2).volume(loudness).play(); }
         new Audio(soundFile3).play();
     }
